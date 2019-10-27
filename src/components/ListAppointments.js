ac657328950a79a55f3452675915929e2024e0e9
jdnav <josedaniel.navarro@edu.uah.es>
2019-10-27 14:13:45 +0100

    Message changes when no appointments

--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -3,27 +3,35 @@ import Appointment from "./Appointment";
 import PropTypes from "prop-types";
 
 //sfc = Stateless Functional Component
-const ListAppointments = ({ appointments, removeAppointment }) => (
-  <div className="card mt-2 py-5">
-    <div className="card-body">
-      <h2 className="card-title text-center">Manage your appointments here</h2>
+const ListAppointments = ({ appointments, removeAppointment }) => {
+  // Print message whether there are no appointments
+  const message =
+    Object.keys(appointments).length === 0
+      ? "No appointments yet"
+      : "Manage appointments here";
 
-      <div className="lista-citas">
-        {appointments.map(appointment => (
-          <Appointment
-            key={appointment.id}
-            appointment={appointment}
-            removeAppointment={removeAppointment}
-          />
-        ))}
+  return (
+    <div className="card mt-2 py-5">
+      <div className="card-body">
+        <h2 className="card-title text-center">{message}</h2>
+
+        <div className="lista-citas">
+          {appointments.map(appointment => (
+            <Appointment
+              key={appointment.id}
+              appointment={appointment}
+              removeAppointment={removeAppointment}
+            />
+          ))}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 ListAppointments.propTypes = {
-    appointments: PropTypes.array.isRequired,
-    removeAppointment: PropTypes.func.isRequired
-}
+  appointments: PropTypes.array.isRequired,
+  removeAppointment: PropTypes.func.isRequired
+};
 
 export default ListAppointments;
